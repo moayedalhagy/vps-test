e71faca6bd7163f3e778a5324ed841c03064c1d7
Moayed Alhagy <118882102+moayedalhagy@users.noreply.github.com>
2024-07-08 13:34:56 +0300

    22

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/',function(){
+
+    return response()->json([
+        'result'=>'ok',
+        'time'=>time()
+    ]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return $request->
 });
