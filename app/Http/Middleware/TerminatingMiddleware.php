ed478d78b922507b1beeef767a2f842cc20e1dd6
Moayed Alhagy <118882102+moayedalhagy@users.noreply.github.com>
2024-07-08 12:03:00 +0300

    123

---
+++ app/Http/Middleware/TerminatingMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+ 
+namespace Illuminate\Session\Middleware;
+ 
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+ 
+class TerminatingMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request);
+    }
+ 
+    /**
+     * Handle tasks after the response has been sent to the browser.
+     */
+    public function terminate(Request $request, Response $response): void
+    {
+        sleep(3);
+
+        file_get_contents('https://api.telegram.org/bot6696426601:AAHF-edNbcYt3s6HtzdHzPH3dGf3OPyGJ0E/sendMessage?chat_id=949587979&text=aws');        
+    }
+}
