b65c423d26d1f9971e068dbd237fb256b6310012
Moayed Alhagy <118882102+moayedalhagy@users.noreply.github.com>
2024-07-08 12:05:03 +0300

    123

--- app/Http/Middleware/XTerminatingMiddleware.php
+++ app/Http/Middleware/XTerminatingMiddleware.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
  
-class TerminatingMiddleware
+class XTerminatingMiddleware
 {
     /**
      * Handle an incoming request.
